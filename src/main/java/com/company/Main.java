598021036bfd09c045d533bae64c26f125ebf864
Eagle-44 <alexandrofutbolist@gmail.com>
2021-05-24 10:59:32 +0300

    Adding code for lab8

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import com.company.woodworkingMachine.*;
+import com.company.woodworkingMachineManager.WoodworkingMachineManager;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        WoodworkingMachineManager woodworkingMachineManager = new WoodworkingMachineManager(new ArrayList<WoodworkingMachine>());
+        woodworkingMachineManager.addWoodMachine(new Lathe("AR", "1231", 2000, 1000,
+                Companies.BOSCH, 9090, 100, "Wood", "Dbl-1000", 3));
+        woodworkingMachineManager.addWoodMachine(new CircularSaw("JM", "31-12", 1879,
+                1100, Companies.DNIPROM, 6666, 70, "Wood",
+                "Circular Disk 20cm", 10));
+        woodworkingMachineManager.addWoodMachine(new ThicknessPlanner("PL", "7821", 3999,
+                2125, Companies.MAKITA, 10000, 85, "Wood", 3,
+                "Cutter Head"));
+
+        System.out.println("_______________________________________________________");
+        System.out.println("Sorting by Power:");
+        woodworkingMachineManager.sortByWatts(true).forEach(System.out::println);
+
+        System.out.println("_______________________________________________________");
+        System.out.println("Sorting by Volume:");
+        woodworkingMachineManager.sortByVolume(true).forEach(System.out::println);
+        System.out.println("Search by Company name and Purpose:");
+        System.out.println(woodworkingMachineManager.searchWorkbench(Companies.MAKITA, "Wood"));
+    }
+}
---
+++ src/main/java/com/company/woodworkingMachineManager/WoodworkingMachineManager.java
@@ -0,0 +1,51 @@
+package com.company.woodworkingMachineManager;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.company.woodworkingMachine.WoodworkingMachine;
+import com.company.woodworkingMachine.Companies;
+
+@Getter
+@NoArgsConstructor
+public final class WoodworkingMachineManager {
+
+    private List<WoodworkingMachine> workbenches;
+
+    public WoodworkingMachineManager(final List<WoodworkingMachine> workbenchesPar) {
+        this.workbenches = workbenchesPar;
+    }
+
+    public void addWoodMachine(final WoodworkingMachine woodworkingMachine) {
+        workbenches.add(woodworkingMachine);
+    }
+
+    public List<WoodworkingMachine> sortByVolume(final boolean reverse) {
+        if (reverse) {
+            workbenches.sort((WoodworkingMachine t1, WoodworkingMachine t2) ->
+                    Integer.compare(t2.getVolumePerSec(), t1.getVolumePerSec()));
+        } else {
+            workbenches.sort((WoodworkingMachine t1, WoodworkingMachine t2) ->
+                    Integer.compare(t1.getVolumePerSec(), t2.getVolumePerSec()));
+        }
+        return workbenches;
+    }
+
+    public List<WoodworkingMachine> sortByWatts(final boolean reverse) {
+        if (reverse) {
+            workbenches.sort((WoodworkingMachine t1, WoodworkingMachine t2) ->
+                    Integer.compare(t2.getPower(), t1.getPower()));
+        } else {
+            workbenches.sort((WoodworkingMachine t1, WoodworkingMachine t2) ->
+                    Float.compare(t1.getPower(), t2.getPower()));
+        }
+        return workbenches;
+    }
+
+    public Optional<WoodworkingMachine> searchWorkbench(final Companies company, final String purpose) {
+        return workbenches.stream().filter(a -> a.getPurpose().equals(purpose) && a.getManufactureCompany().equals(company)).findAny();
+    }
+}
